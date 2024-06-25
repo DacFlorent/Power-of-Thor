284132a065d15cd5264c8af9ddcc661b3206c970
DacFlorent <Dac.florent@gmail.com>
2024-06-25 19:35:03 +0200

    Power of Thor Ok

--- src/Main.java
+++ src/Main.java
@@ -23,30 +23,32 @@ class Player {
         while (true) {
             int remainingTurns = in.nextInt(); // The remaining amount of turns Thor can move. Do not remove this line.
 
+            String direction = "";
+
             if (lightX > initialTx) {
                 System.out.println("E");
             } else if (lightX < initialTx) {
                 System.out.println("W");
             }
 
-            if (lightY < initialTy) {
-                System.out.println("N");
-            } else if (lightY > initialTy) {
-                System.out.println("S");
+            if (initialTy > lightY) {
+                direction += "N";
+                initialTy--;
+            } else if (initialTy < lightY) {
+                direction += "S";
+                initialTy++;
             }
 
-            if (lightX < initialTx && lightY > initialTy) {
-                System.out.println("SW");
-            } else if (lightX > initialTx && lightY < initialTy) {
-                System.out.println("SE");
+            if (initialTx > lightX) {
+                direction += "W";
+                initialTx--;
+            } else if (initialTx < lightX) {
+                direction += "E";
+                initialTx++;
             }
 
-            if (lightX < initialTx && lightY < initialTy) {
-                System.out.println("NW");
-            } else if (lightX > initialTx && lightY > initialTy) {
-                System.out.println("SE");
-            }
             // A single line providing the move to be made: N NE E SE S SW W or NW
+            System.out.println(direction);
             System.err.println("Tour restant : " + remainingTurns);
         }
     }
